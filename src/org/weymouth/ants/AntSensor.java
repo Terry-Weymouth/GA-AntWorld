6b043eb780fcf5a2257352def2eb41509b3d3d6d
Terry Weymouth <weymouth@umich.edu>
2017-01-01 15:27:00 -0500

    Testing Sensor - adding sensor class

---
+++ src/org/weymouth/ants/AntSensor.java
@@ -0,0 +1,45 @@
+package org.weymouth.ants;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AntSensor {
+	
+	Location location = new Location(0,0);
+	final double radius;
+
+	public AntSensor(double r) {
+		radius = r;
+	}
+
+	public void setLocation(Location place) {
+		location = place;
+	}
+
+	public int look(List<Location> spots) {
+		return unboundedLook(filterByBox(spots));
+	}
+
+	private int unboundedLook(List<Location> spots) {
+		if (spots.isEmpty()) {
+			return -1;
+		}
+		int index = 0;		
+		double dist = Util.distance(location, spots.get(index));
+		for (int i = 1; i < spots.size(); i ++) {
+			double d = Util.distance(location, spots.get(i));
+			if (d < dist) {
+				dist = d;
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	private List<Location> filterByBox(List<Location> spots) {
+		List<Location> selected = spots.stream().filter(p -> Util.insideBox(location,radius,p)).collect(Collectors.toList());
+		return selected;
+	}
+
+
+}
