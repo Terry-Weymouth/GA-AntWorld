ff0a5ca7a817854b5601f4f0f6fc0d5716495637
Terry Weymouth <weymouth@umich.edu>
2019-02-22 20:44:36 -0500

    Tweeks to evolution paramaters

--- src/org/weymouth/ants/core/AntWorld.java
+++ src/org/weymouth/ants/core/AntWorld.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class AntWorld {
 	
 	public static final double SENSING_RADIUS = 120.0;
-	public static final int NUMBER_OF_ROUNDS = 2;
+	public static final int NUMBER_OF_ROUNDS = 1;
 	
 	public static final int[] BRAIN_LAYER_WIDTHS = {6,8,7,2};
 	
-	static final int NUMBER_OF_ANTS = 2;
+	static final int NUMBER_OF_ANTS = 1;
 	static final int NUMBER_OF_MEALS = 1000;
 	
 	static final int HEIGHT = 800;
