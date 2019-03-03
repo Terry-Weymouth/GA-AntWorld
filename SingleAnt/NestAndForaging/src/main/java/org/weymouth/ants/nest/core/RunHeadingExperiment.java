889d473d6930aeba25d6b55b72387bf570776641
Terry Weymouth <weymouth@umich.edu>
2019-03-03 13:46:21 -0500

    Finished test of Nest finding behaivor

--- SingleAnt/NestAndForaging/src/main/java/org/weymouth/ants/nest/core/RunHeadingExperiment.java
+++ SingleAnt/NestAndForaging/src/main/java/org/weymouth/ants/nest/core/RunHeadingExperiment.java
@@ -9,6 +9,8 @@ import processing.core.PApplet;
 
 public class RunHeadingExperiment {
 	
+	int baseSpeed = 10;
+	
 	public static void main(String[] args) {
 		AntWorldViewController controller = AntWorldViewController.getController();
 		PApplet.main(AntWorldView.class);
@@ -31,17 +33,49 @@ public class RunHeadingExperiment {
 		int meals = 0;
 		int rounds = 1;
 		AntWorld antNestWorld = new AntWorld(antBrain, ants, meals, rounds);
-		while (seekNest(antNestWorld)){
-			List<Ant> theAnts = antNestWorld.cloneAnts();
-			List<Food> theMeals = antNestWorld.cloneMeals();
-			antNestWorldView.update(theAnts, theMeals);
+		Ant theAnt = antNestWorld.g.getAnts().get(0);
+		for (int i = 0; i < 50; i++) {
+			initAnt(theAnt);
+			while (seekNest(theAnt)){
+				List<Ant> theAnts = antNestWorld.cloneAnts();
+				List<Food> theMeals = antNestWorld.cloneMeals();
+				antNestWorldView.update(theAnts, theMeals);
+			}			
 		}
 	}
 
-	private boolean seekNest(AntWorld antNestWorld) {
-		// TODO Auto-generated method stub
-		return false;
+	private void initAnt(Ant ant) {
+		Location location = Util.randomInteriorPoint();
+		Location lookingAt = Util.randomInteriorPoint();
+		double heading = Util.headingTowards(lookingAt.x, lookingAt.y);
+		ant.heading = heading;
+		ant.location = location;
+		ant.speed = baseSpeed;
+		// System.out.println("Starting - location: " + location + ", heading = " + heading);
 	}
 
-
+	private boolean seekNest(Ant ant) {
+		AntSensor sensor = new AntSensor(ant);
+		double distanceOutput = sensor.nestDistance();
+		double headingOutput = sensor.nestHeading(distanceOutput);
+		double direction = headingOutput - 0.5;
+		double heading = Compass.rewrap(ant.heading + (20.0 * -direction));
+		double speed = baseSpeed;
+		if (distanceOutput < 0.0001) {
+			speed = Math.max(0.0, ant.speed - 2.0);
+		}
+		ant.heading = heading;
+		ant.speed = speed;
+		// System.out.println("Move: heading = " + heading +  ", " + speed);
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		ant.move();
+		if (speed == 0.0) {
+			return false;			
+		}
+		return true;
+	}
 }
